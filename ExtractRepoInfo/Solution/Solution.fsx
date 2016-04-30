d72db61ef560635a13b2413eb5b30fc1f847e02a
Vicenç García Altés <vincentredrum@gmail.com>
2016-04-30 17:08:33 +0100

    Created solution script file

---
+++ ExtractRepoInfo/Solution/Solution.fsx
@@ -0,0 +1,10 @@
+﻿#load "../packages/FsLab/FsLab.fsx"
+#load "../GitLogParser.fs"
+
+open System.IO
+
+let filePath = Path.Combine(__SOURCE_DIRECTORY__, "..\Data\sfa-log.log")
+
+let commits = GitLogParser.getAllCommits filePath
+
+
